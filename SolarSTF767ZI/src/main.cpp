e514f5bc04ce9a490f007e40d68ea0ef0487d97f
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-09-30 12:50:26 +0200

    Hardware changes

--- SolarSTF767ZI/src/main.cpp
+++ SolarSTF767ZI/src/main.cpp
@@ -1,91 +1,160 @@
 #include "mbed.h"
 #include <string>
-#include "MbedJSONValue.h"
 
-Serial pc(USBTX, USBRX,230400); // tx, rx
-Serial device(D1,D0,115200);  // tx, rx
+Serial pc(USBTX, USBRX, 9600); // tx, rx
+Serial device(D1, D0, 115200); // tx, rx
 
-//Timer needed for correcct recieving and sending of information
+//Timer is needed for correcct recieving and sending of information
 Timer timer;
 
-//Light sensors
-AnalogIn a_light_1(A0);
-AnalogIn a_light_2(A1);
-AnalogIn a_light_3(A2);
-AnalogIn a_light_4(A3);
-AnalogIn a_light_5(A4);
-AnalogIn a_light_6(A5);
-AnalogIn a_light_7(PF_8);
-AnalogIn a_light_8(PF_7);
-AnalogIn a_light_9(PF_9);
-//Temperature sensors
-AnalogIn a_temp_1(PF_6);
-AnalogIn a_temp_2(PF_7);
-AnalogIn a_temp_3(PA_0);
-AnalogIn a_temp_4(PA_4);
-AnalogIn a_temp_5(PC_2);
-AnalogIn a_temp_6(PC_3);
-AnalogIn a_temp_7(PC_0);
-AnalogIn a_temp_8(PF_8);
-AnalogIn a_temp_9(PF_9);
-AnalogIn a_bc(PA_0); // battery current
-AnalogIn a_bv(PF_4); //battery voltage
-//start up variable for timer should anlways be unsigned
+DigitalOut LED_1(LED1);
+DigitalOut LED_2(LED2);
+DigitalOut LED_3(LED3);
+
+DigitalOut S0(D9);
+DigitalOut S1(D8);
+DigitalOut S2(D7);
+DigitalOut S3(D6);
+AnalogIn light_sig(A0);
+AnalogIn temp_sig(A1);
+AnalogIn bv_sig(A2);
+
 unsigned long lastms = 0;
 
-int main() {
+string createString(float data)
+{
+    char buffer[10];
+    sprintf(buffer, "%4.0f", data); // datastream value
+    string massage = buffer;
+    return massage;
+};
+
+string createString1(float data)
+{
+    char buffer[10];
+    sprintf(buffer, "%4.1f", data); // datastream value
+    string massage = buffer;
+    return massage;
+};
+
+string createString2(float data)
+{
+    char buffer[10];
+    sprintf(buffer, "%4.2f", data); // datastream value
+    string massage = buffer;
+    return massage;
+};
+
+// main() runs in its own thread in the OS
+int main()
+{
     //Start of the timer
     int begin;
     timer.start();
-    while(1) {
-        MbedJSONValue values;
+    int pin_stat[9][4] = {
+        {1, 1, 1, 0},
+        {0, 0, 0, 1},
+        {1, 0, 0, 1},
+        {0, 1, 0, 1},
+        {1, 1, 0, 1},
+        {0, 0, 1, 1},
+        {1, 0, 1, 1},
+        {0, 1, 1, 1},
+        {1, 1, 1, 1},
+    };
+    LED_1 = 1;
+    LED_2 = 1;
+    LED_3 = 1;
+    while (true)
+    {
+        float data[2][9];
+        float bv, bc;
+        string temp_0, temp_1, temp_2, temp_3, temp_4, temp_5, temp_6, temp_7, temp_8;
+        string light_0, light_1, light_2, light_3, light_4, light_5, light_6, light_7, light_8;
         begin = timer.read_ms();
-        if(begin - lastms >= 1500){
+        if (begin - lastms >= 2000)
+        {
             lastms = begin;
-            char val[5];
-            //Light
-            sprintf(val, "%4.0f", a_light_1/3.28 * 4096);
-            values["data"]["light"][0] = val;
-            sprintf(val, "%4.0f", a_light_2/3.28 * 4096);
-            values["data"]["light"][1] = val;
-            sprintf(val, "%4.0f", a_light_3/3.28 * 4096);
-            values["data"]["light"][2] = val;
-            sprintf(val, "%4.0f", a_light_4/3.28 * 4096);
-            values["data"]["light"][3] = val;
-            sprintf(val, "%4.0f", a_light_5/3.28 * 4096);
-            values["data"]["light"][4] = val;
-            sprintf(val, "%4.0f", a_light_6/3.28 * 4096);
-            values["data"]["light"][5] = val;
-            sprintf(val, "%4.0f", a_light_7/3.28 * 4096);
-            values["data"]["light"][6] = val;
-            sprintf(val, "%4.0f", a_light_8/3.28 * 4096);
-            values["data"]["light"][7] = val;
-            //Temperature
-            sprintf(val, "%4.1f", a_temp_1/3.28 * 4096);
-            values["data"]["temp"][0] = val;
-            sprintf(val, "%4.1f", a_temp_2/3.28 * 4096);
-            values["data"]["temp"][1] = val;
-            sprintf(val, "%4.1f", a_temp_3/3.28 * 4096);
-            values["data"]["temp"][2] = val;
-            sprintf(val, "%4.1f", a_temp_4/3.28 * 4096);
-            values["data"]["temp"][3] = val;
-            sprintf(val, "%4.1f", a_temp_5/3.28 * 4096);
-            values["data"]["temp"][4] = val;
-            sprintf(val, "%4.1f", a_temp_6/3.28 * 4096);
-            values["data"]["temp"][5] = val;
-            sprintf(val, "%4.1f", a_temp_7/3.28 * 4096);
-            values["data"]["temp"][6] = val;
-            sprintf(val, "%4.1f", a_temp_8/3.28 * 4096);
-            values["data"]["temp"][7] = val;
-            // BV,BC
-            sprintf(val, "%4.2f", a_bv * 3.28 + 1000*(float)a_bv * 3.28/200);
-            values["data"]["bv"] = val;
-            sprintf(val, "%4.2f", a_bc/3.28);
-            values["data"]["bc"] = val;
-            //send data via serial port
-            std::string data = values.serialize();
-            pc.printf("%s\n", data);
-            device.printf("%s\n", data);
+            for (int i = 0; i < 9; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    if (c == 0)
+                    {
+                        S0 = pin_stat[i][c];
+                    }
+                    if (c == 1)
+                    {
+                        S1 = pin_stat[i][c];
+                    }
+                    if (c == 2)
+                    {
+                        S2 = pin_stat[i][c];
+                    }
+                    if (c == 3)
+                    {
+                        S3 = pin_stat[i][c];
+                    }
+                }
+                if (i == 0)
+                {
+                    temp_0 = createString1(temp_sig * 4.5 * 100);
+                    light_0 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 1)
+                {
+                    temp_1 = createString1(temp_sig * 4.5 * 100);
+                    light_1 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 2)
+                {
+                    temp_2 = createString1(temp_sig * 4.5 * 100);
+                    light_2 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 3)
+                {
+                    temp_3 = createString1(temp_sig * 4.5 * 100);
+                    light_3 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 4)
+                {
+                    temp_4 = createString1(temp_sig * 4.5 * 100);
+                    light_4 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 5)
+                {
+                    temp_5 = createString1(temp_sig * 4.5 * 100);
+                    light_5 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 6)
+                {
+                    temp_6 = createString1(temp_sig * 4.5 * 100);
+                    light_6 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 7)
+                {
+                    temp_7 = createString1(temp_sig * 4.5 * 100);
+                    light_7 = createString(light_sig / 3.28 * 4096);
+                }
+                if (i == 8)
+                {
+                    temp_8 = createString1(temp_sig * 4.5 * 100);
+                    light_8 = createString(light_sig / 3.28 * 4096);
+                }
+            }
+            bc = bv_sig * 3.3 / 220;
+            bv = bc * (982 + 200);
+            string post1 = "{ \"data\": {\"bv\":" + createString2(bv * 10) + ",\"bc\":" + createString2(bc * 1000);
+            string post2 = ",\"light\":[" + light_0 + "," + light_1 + "," + light_2 + "," + light_3 + ",";
+            string post3 = light_4 + "," + light_5 + "," + light_6 + "," + light_7 + "," + light_8;
+            string post4 = "],\"temp\":[" + temp_0 + "," + temp_1 + "," + temp_2 + "," + temp_3 + ",";
+            string post5 = temp_4 + "," + temp_5 + "," + temp_6 + "," + temp_7 + "," + temp_8 + "]} }";
+            pc.printf("%s\n", post1 + post2 + post3);
+            device.printf("%s", post1);
+            device.printf("%s", post2);
+            device.printf("%s", post3);
+            device.printf("%s", post4);
+            device.printf("%s", post5);
         }
     }
 }
--- esp8266/NodeMCU.ino
+++ esp8266/NodeMCU.ino
@@ -2,43 +2,48 @@
 #include <ESP8266HTTPClient.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial NodeSerial(D5, D6);  //RX, TX
+SoftwareSerial NodeSerial(D5, D6); //RX, TX
 
-const char* ssid = "NodeMCU";
-const char* password = "1q2w120195";
+const char *ssid = "NodeMCU";
+const char *password = "1q2w120195";
 
-void setup () {
-    pinMode(D5, INPUT);
-    pinMode(D6, OUTPUT);
-    Serial.begin(230400);
-    NodeSerial.begin(115200);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+void setup()
+{
+  pinMode(D5, INPUT);
+  pinMode(D6, OUTPUT);
+  Serial.begin(9600);
+  NodeSerial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.print("Connecting..");
   }
 }
 
-void sendData(String data) {
+void sendData(String data)
+{
   //Check WiFi connection status
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     HTTPClient http;
     http.begin("http://blooming-fortress-61113.herokuapp.com/data");
     http.addHeader("Content-Type", "application/json");
     String postMessage = data;
     //Uncomment these lines to see output
-    /*
-      Serial.println("Data:");
-      Serial.println(data);
-      Serial.println("PostMessage:");
-      Serial.println(postMessage);
-    */
+
+    Serial.println("Data:");
+    Serial.println(data);
+    Serial.println("PostMessage:");
+    Serial.println(postMessage);
+
     //Send POST request
     int httpCode = http.POST(postMessage);
     //Check the returning code
-    if (httpCode > 0) {
-    //Print http response
-    //Serial.println("http response: " + httpCode);
+    if (httpCode > 0)
+    {
+      //Print http response
+      //Serial.println("http response: " + httpCode);
       //Get the request response payload
       String payload = http.getString();
       //Print the response payload
@@ -51,11 +56,13 @@ void sendData(String data) {
   //delay(100);
 }
 
-void loop() {
+void loop()
+{
   String message;
-    if (NodeSerial.available()>0) {
-      String message = NodeSerial.readString();
-      Serial.println(message);
-      sendData(message);
-    }
+  if (NodeSerial.available() > 0)
+  {
+    String message = NodeSerial.readString();
+    Serial.println(message);
+    sendData(message);
+  }
 }
